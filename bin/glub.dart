22e8ed221825ebe1700f63acc32517ba6dd8d8ee
Kaio Delphino <dash.zet@gmail.com>
2024-08-16 13:19:10 -0300

    initial commit

---
+++ bin/glub.dart
@@ -0,0 +1,5 @@
+import 'package:glub/glub.dart' as glub;
+
+void main(List<String> arguments) {
+  print('Hello world: ${glub.calculate()}!');
+}
---
+++ test/glub_test.dart
@@ -0,0 +1,8 @@
+import 'package:glub/glub.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
